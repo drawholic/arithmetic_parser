8eeec0ef99ae57bc16113f01cc02edf210169621
drawholka <mamrokhavlad@gmail.com>
2025-02-05 17:40:49 +0200

    Scanner definition. Future comments/documentation required.

---
+++ src/Scanner.cpp
@@ -0,0 +1,50 @@
+#include "Scanner.hpp"
+
+
+
+Scanner::Scanner(std::string& buf): buffer(buf) {};
+
+
+void Scanner::remove_whitespaces()
+{
+	std::string temp(buffer);
+	str_iter new_end = std::remove(temp.begin(), temp.end(), ' ');
+	buffer.assign(temp.begin(), new_end);
+};
+
+str_iter Scanner::get_number_end(str_iter start, str_iter buf_end)
+{
+	char c = *start;
+	while(std::isdigit(c) == 0 && start != buf_end)
+	{
+		start = std::next(start, 1);
+		c = *start;
+	};
+	return start;
+};
+
+void Scanner::split_tokens()
+{
+	str_iter it = buffer.begin();
+	str_iter pend = buffer.end();
+	str_iter num_end;
+	std::string temp;
+
+	char c;
+	while(it != pend)
+	{
+		c = *it;
+		if(std::isdigit(c))
+		{
+			num_end = get_number_end(it, pend);
+			temp.assign(it, num_end);
+			str_tokens.push_back(temp);
+			it = num_end;
+		}else
+		{
+			temp.assign(it, std::next(it));
+			str_tokens.push_back(temp);
+			it++;
+		};
+	};
+};
