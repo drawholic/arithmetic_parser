4be0b08a9bf2691f8cfa15cc92c6dde953d09a06
drawholka <mamrokhavlad@gmail.com>
2025-02-06 09:12:10 +0200

    NonTerminal print override and string constructor

--- include/NonTerminal.hpp
+++ include/NonTerminal.hpp
@@ -3,6 +3,7 @@
 
 #include "AbstractExpression.hpp"
 #include <cmath>
+#include <string>
 
 enum Operator
 {
@@ -34,6 +35,10 @@ public:
 	 */
 	NonTerminal(AbstractExpression*, AbstractExpression*, Operator);
 
+
+	NonTerminal(std::string&);
+
+
 	/**
 	 * Calculates a value of an expression based on an op attribute (might change the type to float with possible type conversion)
 	 * 
@@ -49,5 +54,8 @@ public:
 	 * @return result of evaluation
 	 */
 	int evaluate(int, int) const;
+
+	void print() override;
+
 };
 #endif
