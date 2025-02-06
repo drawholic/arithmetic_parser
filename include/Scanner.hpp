5d239e41b5b28788f499681353ba5a7b52fb5f25
drawholka <mamrokhavlad@gmail.com>
2025-02-06 09:13:26 +0200

    Scanner attributes and methods for string tokens to actual tokens conversion

--- include/Scanner.hpp
+++ include/Scanner.hpp
@@ -2,18 +2,23 @@
 #define SCANNER_HPP
 
 #include "AbstractExpression.hpp"
+#include "NonTerminal.hpp"
+#include "Terminal.hpp"
 #include <algorithm>
 #include <vector>
 #include <string>
 
 typedef std::vector<std::string> vec_str;
 typedef std::string::iterator str_iter;
+typedef std::vector<AbstractExpression*> v_tokens;
 
 class Scanner
 {
 	std::string buffer;
 	vec_str str_tokens;
+	v_tokens tokens;
 	str_iter get_number_end(str_iter, str_iter);
+	void convert_to_tokens();
 
 public:
 	Scanner(std::string& buf);
